d18694fc3b287168b90b240c29beb4dd27a181ca
ilonaverenich <35536824+ilonaverenich@users.noreply.github.com>
2022-01-12 15:11:49 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,23 +5,24 @@ let inputValue = document.querySelector('.inputValue');
 
 function del (){ 
   let num = +document.querySelector('.input').value
-  console.log(num)
+
   if (!isNaN(num)){
    let result =[];
    for (let i=1; i<=num; i++){
       if (num % i == 0){
          result.push(i)
       } 
-     
    }
 
    inputValue.innerHTML =`Делители числа ${num}:  ${result}` ;
+  input.value =''
   }
   else{
    inputValue.innerHTML =`Вы ввели некорректные данные!` ;
   }
- 
 }
+
+
 
 
 
